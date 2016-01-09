76d1eddfec18bf20ea2cc7c076af50d114c93817
Felipe Lima <felipelima@MBPFH.local>
2016-01-08 20:38:22 -0800

    Refactored code.

--- led.py
+++ led.py
@@ -1,19 +1,37 @@
+#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-
 print GPIO.VERSION
 
-GPIO.setmode(GPIO.BOARD)
+# LED CONFIG - Set GPIO Ports
+LED_1 = 26
 
+# LED Pool
+LEDS = [LED_1]
 
-GPIO.setup(26, GPIO.OUT)
+def setup():
+  # Set up the wiring
+  GPIO.setmode(GPIO.BOARD)
+  # Setup Ports
+  for led_id in LEDS:
+    GPIO.setup(led_id, GPIO.OUT)
 
-switch = False
+def clear():
+  for led_id in LEDS:
+    GPIO.output(led_id, 0)
 
-delays = [1,2,3,4,5]
-for delay in delays:
-  switch = not switch
-  GPIO.output(26, switch)
-  time.sleep(delay)
+def magic():
+  switch = False
+  delays = [1,2,3,4,5]
 
-GPIO.cleanup()
+  for delay in delays:
+      switch = not switch
+      GPIO.output(LED1, switch)
+      time.sleep(delay)
+
+def main():
+  setup()
+  clear()
+  magic()
+  clear()
+  GPIO.cleanup()
